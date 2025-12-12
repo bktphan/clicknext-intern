415696feedd942002ee8f51c107afa112175ae2e
bktphan <bktphan.me@gmail.com>
2025-12-12 16:43:30 +0700

    feat: Create the 'LoginView.vue' file for login page

--- banking-website-app/src/router/index.js
+++ banking-website-app/src/router/index.js
@@ -1,8 +1,19 @@
+import LoginView from '@/views/LoginView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+  ],
 })
 
 export default router
